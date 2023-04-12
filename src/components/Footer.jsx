bf56e3dfdc370b516c2a87efb52c719956af9c3a
Lupercal13 <hpayne13@gmail.com>
2023-04-12 22:56:09 +0900

    add footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-black/70 h-[200px]'>
+        <div className='grid text-white'>
+            <div className='pl-2 flex flex-col gap-4'>
+
+            <p>Email:</p>
+            <p>phone:</p>
+            <p>Address:</p>
+            <p>Socials:</p>
+            <p>Copyright:</p>
+            <p className='text-gray-500'>developed by:</p>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
