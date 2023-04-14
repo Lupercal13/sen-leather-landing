683403b5e60a70475f1b9f556f86fbf61221b0f2
Lupercal13 <hpayne13@gmail.com>
2023-04-14 16:19:39 +0900

    updates to gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,39 +1,55 @@
-import React from "react";
-import { Navigation, Pagination } from "swiper";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade"
 
-import blackWallet from '../assets/black-wallets.jpg'
-import orangeWallet from '../assets/orange-wallet.jpg'
-import handCleanerCase from '../assets/hand-cleaner-holder.jpg'
-import hairTies from '../assets/hair-ties.jpg'
+import { Navigation, Pagination, EffectFade } from "swiper";
+
+import blackWallet from "../assets/black-wallets.jpg";
+import orangeWallet from "../assets/orange-wallet.jpg";
+import handCleanerCase from "../assets/hand-cleaner-holder.jpg";
+import hairTies from "../assets/hair-ties.jpg";
 
 const Gallery = () => {
   return (
-    <div className="" id="gallery">
-        <div className="text-center text-5xl font-semibold border-b-4 pb-4 mx-6 ">
-            <h1>Our Works</h1>
-        </div>
+    <div className="my-20" id="gallery">
+      <div className="text-center text-5xl font-semibold border-b-4 pb-4 mx-6 ">
+        <h1>Our Works</h1>
+      </div>
 
-    <Swiper
-    className=""
-      modules={[Navigation, Pagination]}
-      navigation
-      autoHeight
-      pagination={{ clickable: true }}
-      spaceBetween={10}
-      slidesPerView={1}
-      onSlideChange={() => console.log("slide Change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide><img className="rounded "  src={blackWallet}/><p></p></SwiperSlide>
-      <SwiperSlide><img className="rounded " src={orangeWallet}/></SwiperSlide>
-      <SwiperSlide><img className="rounded " src={handCleanerCase}/></SwiperSlide>
-      <SwiperSlide><img className="rounded " src={hairTies}/></SwiperSlide>
-
-    </Swiper>
+      <Swiper
+      style={{"--swiper-navigation-color": "#fff",
+    "--swiper-pagination-color": "#000"}}
+        className=""
+        // loop='true'
+        modules={[Navigation, Pagination, EffectFade]}
+        navigation
+        autoHeight
+        pagination={{ clickable: true }}
+        spaceBetween={10}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide Change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <img className="rounded " src={blackWallet} />
+            <p className="text-sm text-center tracking-wide">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ut est eum harum error rem amet commodi sunt officiis rerum.</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="rounded " src={orangeWallet} />
+          <p className="text-sm text-center tracking-wide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, incidunt! Quia laudantium veniam nesciunt, rerum optio ut error quod eaque molestias. Excepturi fuga totam error repellat. Incidunt dolores odio qui?</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="rounded " src={handCleanerCase} />
+          <p className="text-sm text-center tracking-wide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, eos?</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="rounded " src={hairTies} />
+          <p className="text-sm text-center tracking-wide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo amet, incidunt ex facilis quidem delectus.</p>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
